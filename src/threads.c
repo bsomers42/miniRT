9d5025593d1dd4b27e2dbca6a08b86fd66cd4b39
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-10-19 18:22:33 +0200

    made norm

--- src/threads.c
+++ src/threads.c
@@ -6,14 +6,13 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/17 13:15:51 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/10/19 15:04:26 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/10/19 17:25:24 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 #include "threads.h"
 #include <unistd.h>
-#include <stdio.h> //remove
 
 void	check_done(t_threadinfo *info)
 {
@@ -31,8 +30,10 @@ void	check_done(t_threadinfo *info)
 static void	fill_pixel(t_threadinfo *info, int x, int y)
 {
 	t_color	color;
+	t_list	*lst_sphere;
 
-	color = antialias_color(info->data->parse->lst_sphere, info->data->ray, x, y);
+	lst_sphere = info->data->parse->lst_sphere;
+	color = antialias_color(lst_sphere, info->data->ray, x, y);
 	put_color(info, x, y, color);
 	if (pthread_mutex_lock(&(info->data->pixel_lock)) != 0)
 		error_exit("pthread_mutex_lock", 1);
