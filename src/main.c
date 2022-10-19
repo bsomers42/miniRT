6cf1b5393845568bc130575b56b084b2798a431c
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-10-19 18:22:10 +0200

    changed order parse_map

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bsomers <bsomers@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/22 14:54:42 by bsomers       #+#    #+#                 */
-/*   Updated: 2022/10/19 15:46:35 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/10/19 17:27:57 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,48 +32,6 @@ void	minirt_close(void *ptr)
 	(void)ptr;
 	exit(EXIT_SUCCESS);
 }
-
-// t_sphere	*init_spheres(void)
-// {
-// 	t_sphere	*spheres;
-
-// 	spheres = malloc((4) * sizeof(t_sphere));
-// 	if (spheres == NULL)
-// 		error_exit("malloc failed!\n", 1);
-// 	/* sphere 1 */
-// 	spheres[0].center.x = 0.0;
-// 	spheres[0].center.y = 0.0;
-// 	spheres[0].center.z = -1.0;
-// 	spheres[0].radius = 0.5;
-// 	spheres[0].color.r = 255;
-// 	spheres[0].color.g = 0;
-// 	spheres[0].color.b = 0;
-// 	/* sphere 2 */
-// 	spheres[1].center.x = 0.0;
-// 	spheres[1].center.y = -100.5;
-// 	spheres[1].center.z = -1.0;
-// 	spheres[1].radius = 100;
-// 	spheres[1].color.r = 0;
-// 	spheres[1].color.g = 255;
-// 	spheres[1].color.b = 0;
-// 	/* sphere 3 */
-// 	spheres[2].center.x = 0.0;
-// 	spheres[2].center.y = -0.5;
-// 	spheres[2].center.z = -1.0;
-// 	spheres[2].radius = 0.5;
-// 	spheres[2].color.r = 0;
-// 	spheres[2].color.g = 0;
-// 	spheres[2].color.b = 255;
-// 	/* sphere 4 */
-// 	spheres[3].center.x = 0.3;
-// 	spheres[3].center.y = 0.3;
-// 	spheres[3].center.z = -1.5;
-// 	spheres[3].radius = 0.5;
-// 	spheres[3].color.r = 255;
-// 	spheres[3].color.g = 255;
-// 	spheres[3].color.b = 0;
-// 	return (spheres);
-// }
 
 t_ray	init_ray(void)
 {
@@ -106,6 +64,7 @@ void	init_infos(t_data *data, t_threadinfo **infos)
 
 void	init_data(t_data *data, char **argv)
 {
+	data->parse = parse_map(argv);
 	data->mlx_str.mlx = mlx_init(WIDTH, HEIGHT, "MickeyRT", true);
 	if (data->mlx_str.mlx == NULL)
 		error_exit("mlx_init failed!\n", 1);
@@ -113,9 +72,7 @@ void	init_data(t_data *data, char **argv)
 	if (data->mlx_str.img == NULL)
 		error_exit("mlx_new_image failed!\n", 1);
 	data->pixels_done = 0;
-	//data->spheres = init_spheres();
 	data->ray = init_ray();
-	data->parse = parse_map(argv);
 	pthread_mutex_init(&(data->pixel_lock), NULL);
 	pthread_mutex_init(&(data->mlx_lock), NULL);
 }
@@ -145,10 +102,6 @@ int	main(int argc, char *argv[])
 	mlx_key_hook(data.mlx_str.mlx, &minirt_keyhook, &data.mlx_str);
 	mlx_close_hook(data.mlx_str.mlx, &minirt_close, NULL);
 	mlx_image_to_window(data.mlx_str.mlx, data.mlx_str.img, 0, 0);
-	t_sphere	*sphere;
-	sphere = (t_sphere *)(infos[0].data->parse->lst_sphere->content);
-	printf("incoming center: (%f, %f, %f)\n", sphere->center.x, sphere->center.y, sphere->center.z);
-	printf("incoming color: (%d, %d, %d)\n", sphere->color.r, sphere->color.g, sphere->color.b);
 	draw_loading_bar();
 	make_threads(&infos);
 	mlx_loop(data.mlx_str.mlx);
