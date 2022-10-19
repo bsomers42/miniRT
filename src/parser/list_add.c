374384785d41c45429b6236c07ae063a2a522922
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-10-19 16:17:01 +0200

    cast sp to void*

--- src/parser/list_add.c
+++ src/parser/list_add.c
@@ -6,7 +6,7 @@
 /*   By: bsomers <bsomers@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/11 11:13:12 by bsomers       #+#    #+#                 */
-/*   Updated: 2022/10/19 12:50:40 by bsomers       ########   odam.nl         */
+/*   Updated: 2022/10/19 15:19:45 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,36 @@ float	ft_stofl(char *str);
 void	ft_lstadd_sp(t_list **lst, char **line)
 {
 	t_sphere	sp;
+	t_sphere	*sp_void;//jma
 	char    **tmp;
-	t_list   *node;
+	// t_list   *node;
 
-	node = (t_list *)malloc(sizeof(t_list));
+	// node = (t_list *)malloc(sizeof(t_list));
 	tmp = ft_split(line[1], ',');
 	sp.x = ft_stofl(tmp[0]);
 	sp.y = ft_stofl(tmp[1]);
 	sp.z = ft_stofl(tmp[2]);
+	sp.center.x = sp.x; //jma
+	sp.center.y = sp.y; //jma
+	sp.center.z = sp.z; //jma
+	printf("[x: %f, y: %f, z: %f]\n", sp.x, sp.y, sp.z);//jma
 	free_array(tmp);
 	sp.diam = ft_stofl(line[2]);
 	tmp = ft_split(line[3], ',');
 	sp.r = ft_atoi(tmp[0]);
 	sp.g = ft_atoi(tmp[1]);
 	sp.b = ft_atoi(tmp[2]);
+	sp.color.r = sp.r * 1.0; //jma
+	sp.color.g = sp.g * 1.0; //jma
+	sp.color.b = sp.b * 1.0; //jma
 	printf("[r: %d, g: %d, b: %d]\n", sp.r, sp.g, sp.b);
+	sp_void = malloc(sizeof(t_sphere));//jma
+	//check failed //jma
+	*sp_void = sp;//jma
 	free_array(tmp);
-	node->content = &sp;
-	node->next = NULL;
-	ft_lstadd_back(lst, node);
+	// node->content = &sp;
+	// node->next = NULL;
+	ft_lstadd_back(lst, ft_lstnew(sp_void));//node); //jma
 	free_array(line);
 }
 
