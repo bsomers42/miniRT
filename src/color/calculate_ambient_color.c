2e9a2d64b37087dce3f713a883cdb731b14cc238
Jaberkro <jaberkro@student.codam.nl>
2022-11-03 22:57:40 +0100

    this function will not be used anymore

--- src/color/calculate_ambient_color.c
+++ src/color/calculate_ambient_color.c
@@ -6,12 +6,13 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/26 10:48:49 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/11/02 11:36:07 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/11/03 22:55:40 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+//this function is not used anymore
 t_color	calculate_ambient_color(t_color color, t_amb *amb)
 {
 	t_color	result_color;
@@ -23,7 +24,7 @@ t_color	calculate_ambient_color(t_color color, t_amb *amb)
 	// ambt->g = 255;//temporary
 	// ambt->b = 255;//temporary
 	// ambt->ratio = 1.0; //temporary
-	result_color.r = (float)color.r * (float)amb->r / 255.0 * amb->ratio;
+	result_color.r = (float)color.g * (float)amb->r / 255.0 * amb->ratio;
 	result_color.g = (float)color.g * (float)amb->g / 255.0 * amb->ratio;
 	result_color.b = (float)color.b * (float)amb->b / 255.0 * amb->ratio;
 	return (result_color);
