57c9caeb2cb6e93cb60d641194f5337b8704fe6d
bsomers42 <brittsomers22@gmail.com>
2022-11-07 12:29:05 +0100

    Adjusting to make plane dir work

--- src/hit/hit_cylinder.c
+++ src/hit/hit_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: bsomers <bsomers@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/03 11:37:15 by bsomers       #+#    #+#                 */
-/*   Updated: 2022/11/04 16:17:53 by bsomers       ########   odam.nl         */
+/*   Updated: 2022/11/07 12:21:29 by bsomers       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,8 @@ t_point	calc_cap_center(t_cyl *cyl, int side)
 		// tmp.dir.z = tmp.dir.z * -1;
 
 		cap_center = ray_at(tmp, (cyl->height /2 * -1 ));
+		// cap_center = ray_at(tmp, (cyl->height /2 * -1 ));
+
 	}
 	// ft_printf("Old center: [%f][%f][%f]\nNew center: [%f][%f][%f]\n", \
 	// orig_center.x, orig_center.y, orig_center.z, cap_center.x, cap_center.y, cap_center.z);
@@ -55,19 +57,12 @@ int	intersect_circle(t_cyl *cyl, t_ray ray, float t, t_point cap_center)
 	float	d;
 
 	radius = cyl->diam / 2;
-	p = add_points(ray.origin, normalize_point(ray.dir));
-	p = multiply_point_float(p, t);
-	// p = ray_at(ray, t);
+	// p = add_points(ray.origin, normalize_point(ray.dir));
+	// p = multiply_point_float(p, t);
+	p = ray_at(ray, t);
 	v = substract_points(p, cap_center);
 	d = dot_points(v, v);
 	return (sqrtf(d) <= radius);
-	// if (sqrtf(d) <= radius)
-	// {
-	// 	printf("Returning 1\n");
-	// 	return (1);
-	// }
-	// printf("returning 0\n");
-	// return (0);
 }
 
 int	hit_cap(t_cyl *cyl, t_ray ray, float t_min, float t_max, t_besthit *hit_rec, t_point cap_center)
@@ -85,8 +80,9 @@ int	hit_cap(t_cyl *cyl, t_ray ray, float t_min, float t_max, t_besthit *hit_rec,
 	if (denom > t_min)
 	{
 		polo = substract_points(cap_center, ray.origin);
+		// polo = multiply_points(polo, n);
 		t = dot_points(polo, n) / denom;
-		if (t >= 0 && t < t_max)
+		if (t >= t_min && t < t_max)
 		{
 			if (intersect_circle(cyl, ray, t, cap_center))
 			{
@@ -97,32 +93,42 @@ int	hit_cap(t_cyl *cyl, t_ray ray, float t_min, float t_max, t_besthit *hit_rec,
 
 				hit_rec->color = cyl->color;
 				hit_rec->center = cyl->center; //deze ook aanpassen naar cap center?
-				hit_rec->normal = n;
+				if (dot_points(ray.dir, n) < 0)
+				{
+					hit_rec->front_face = 1;
+					hit_rec->normal = n;
+				}
+				else
+				{
+					hit_rec->front_face = 0;
+					hit_rec->normal = multiply_point_float(n, -1.0);
+				}
 				return (1);
 			}
 		}
 	}
-	n = normalize_point(multiply_point_float(cyl->dir, -1.0));
-	// t = 0;
-	denom = dot_points(n, normalize_point(ray.dir));
-	if (denom > t_min)
-	{
-		polo = substract_points(cap_center, ray.origin);
-		t = dot_points(polo, n) / denom;
-		if (t >= 0 && t < t_max)
-		{
-			if (intersect_circle(cyl, ray, t, cap_center))
-			{
-				hit_rec->t = t;
-				hit_rec->hit_point = ray_at(ray, hit_rec->t);
-				hit_rec->color = cyl->color;
-				// hit_rec->color = new_color(0,255,255);
-				hit_rec->center = cyl->center; //deze ook aanpassen naar cap center?
-				hit_rec->normal = n;
-				return (1);
-			}
-		}
-	}
+	// n = normalize_point(multiply_point_float(cyl->dir, -1.0));
+	// // t = 0;
+	// denom = dot_points(n, normalize_point(ray.dir));
+	// if (denom > t_min)
+	// {
+	// 	polo = substract_points(cap_center, ray.origin);
+	// 	// polo = multiply_points(polo, n);
+	// 	t = dot_points(polo, n) / denom;
+	// 	if (t >= 0 && t < t_max)
+	// 	{
+	// 		if (intersect_circle(cyl, ray, t, cap_center))
+	// 		{
+	// 			hit_rec->t = t;
+	// 			hit_rec->hit_point = ray_at(ray, hit_rec->t);
+	// 			hit_rec->color = cyl->color;
+	// 			// hit_rec->color = new_color(0,255,255);
+	// 			hit_rec->center = cyl->center; //deze ook aanpassen naar cap center?
+	// 			hit_rec->normal = n;
+	// 			return (1);
+	// 		}
+	// 	}
+	// }
 
 	return (0);
 }
@@ -149,7 +155,7 @@ int	hit_any_cylinder(t_parse map_info, t_ray ray, t_besthit *hit_rec, float t_mi
 			hit_anything = i;
 			t_max = tmp_rec.t;
 			*hit_rec = tmp_rec;
-			hit_rec->color = new_color(0,255,255);
+			hit_rec->color = new_color(255,255,255);
 		}
 		tmp_center = calc_cap_center(cyl, 1);
 		if (hit_cap(cyl, ray, t_min, t_max, &tmp_rec, tmp_center))
