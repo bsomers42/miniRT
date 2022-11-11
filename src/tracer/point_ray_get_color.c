db1fdddb993b25bb2be8b8eefe869fbe1ece737e
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-11-11 14:36:12 +0100

    updated descriptions, more norm

--- src/tracer/point_ray_get_color.c
+++ src/tracer/point_ray_get_color.c
@@ -6,13 +6,12 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/06 13:26:28 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/11/11 13:17:58 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/11/11 14:32:06 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 #include <math.h>
-#include <stdio.h>
 
 t_point	ray_at(t_ray ray, float t)
 {
@@ -25,13 +24,13 @@ t_point	ray_at(t_ray ray, float t)
 }
 
 /**
- * @brief calculate if this pixel can see the light or is in the shadow. In the
- * case of shadow, color is black. Else, the color will be a gradient between
- * black and full color of the shape.
+ * @brief If this pixel is in the shadow, calculate the ambient light at this
+ *  pixel. If not in the shadow, calculate the combination of ambient and 
+ * normal light coming together at this pixel.
  * 
- * @param map_info 
- * @param record 	closest shape to the camera, hit by the ray
- * @return t_color 
+ * @param map_info 	parsed info from .rt map
+ * @param record 	information on the object closest to the ray origin
+ * @return t_color 	the shaded color
  */
 t_color	calculate_shadow_shade(t_parse map_info, t_besthit record)
 {
@@ -40,34 +39,24 @@ t_color	calculate_shadow_shade(t_parse map_info, t_besthit record)
 	t_besthit	not_needed;
 	t_color		color;
 	double		cosangle;
-	t_point		light; //temporary
-	double		brightness; //temporary
-	t_unit_color	dcolor;
 
-	light = map_info.light.origin; //temporary
-	brightness = (double)map_info.light.bright; //temporary
 	light_ray.origin = record.hit_point;
-	light_ray.dir = substract_points(light, light_ray.origin); //temporary this is actually lightray length
+	light_ray.dir = substract_points(map_info.light.origin, light_ray.origin); //temporary this is actually lightray length
 	light_len = sqrt((float)dot_points(light_ray.dir, light_ray.dir));
 	light_ray.dir = normalize_point(light_ray.dir);
-	if (hit_anything(map_info, light_ray, &not_needed, 0.01, light_len) > 0)
+	if (hit_anything(map_info, light_ray, &not_needed, 0.01, light_len) > 0) // or inside of shape?
 	{
-		// calculate shadow color
-		color.r = ((double)record.color.r / 255.0) / (double)M_PI * ((double)brightness + ((double)map_info.amb.color.r * map_info.amb.ratio / 255.0)) * 255.0;
-		color.g = ((double)record.color.g / 255.0) / (double)M_PI * ((double)brightness + ((double)map_info.amb.color.g * map_info.amb.ratio / 255.0)) * 255.0;
-		color.b = ((double)record.color.b / 255.0) / (double)M_PI * ((double)brightness + ((double)map_info.amb.color.b * map_info.amb.ratio / 255.0)) * 255.0;
+		color.r = ((float)record.color.r / 255.0) / (float)M_PI * ((float)(float)map_info.light.bright + ((float)map_info.amb.color.r * map_info.amb.ratio / 255.0)) * 255.0;
+		color.g = ((float)record.color.g / 255.0) / (float)M_PI * ((float)(float)map_info.light.bright + ((float)map_info.amb.color.g * map_info.amb.ratio / 255.0)) * 255.0;
+		color.b = ((float)record.color.b / 255.0) / (float)M_PI * ((float)(float)map_info.light.bright + ((float)map_info.amb.color.b * map_info.amb.ratio / 255.0)) * 255.0;
 		return (color);
-		// return (new_color(255, 255, 255));
 	}	
-	cosangle = (double)dot_points(record.normal, light_ray.dir);
+	cosangle = (float)dot_points(record.normal, light_ray.dir);
 	if (cosangle < 0.0)
 		cosangle = 0.0;
-	//calculate: normalized record.color / pi * light brightness * (cosangle + ambiant)
-	dcolor.r = (((double)record.color.r / 255.0) / (double)M_PI * ((double)brightness + ((double)cosangle + (double)map_info.amb.color.r * map_info.amb.ratio / 255.0)));
-	dcolor.g = (((double)record.color.g / 255.0) / (double)M_PI * ((double)brightness + ((double)cosangle + (double)map_info.amb.color.g * map_info.amb.ratio / 255.0)));
-	dcolor.b = (((double)record.color.b / 255.0) / (double)M_PI * ((double)brightness + ((double)cosangle + (double)map_info.amb.color.b * map_info.amb.ratio / 255.0)));
-	//normalized color to color with value 0-255
-	color = new_color((unsigned int)((double)dcolor.r * 255.0), (unsigned int)((double)dcolor.g * 255.0), (unsigned int)((double)dcolor.b * 255.0));
+	color.r = (((float)record.color.r / 255.0) / (float)M_PI * ((float)(float)map_info.light.bright + ((float)cosangle + (float)map_info.amb.color.r * map_info.amb.ratio / 255.0))) * 255.0;
+	color.g = (((float)record.color.g / 255.0) / (float)M_PI * ((float)(float)map_info.light.bright + ((float)cosangle + (float)map_info.amb.color.g * map_info.amb.ratio / 255.0))) * 255.0;
+	color.b = (((float)record.color.b / 255.0) / (float)M_PI * ((float)(float)map_info.light.bright + ((double)cosangle + (double)map_info.amb.color.b * map_info.amb.ratio / 255.0))) * 255.0;
 	return (color);
 }
 
@@ -75,7 +64,7 @@ t_color	calculate_shadow_shade(t_parse map_info, t_besthit record)
  * @brief Get the color is this ray. If you hit nothing, color is black. Else,
  * the color is the shaded version of the color of the shape closest to you. 
  * 
- * @param map_info 
+ * @param map_info parsed info from .rt map
  * @param ray 
  * @return t_color 
  */
@@ -89,8 +78,6 @@ t_color	get_ray_color(t_parse map_info, t_ray ray)
 	if (closest_index > 0)
 	{
 		color = calculate_shadow_shade(map_info, record);
-
-		//real color without shading
 		// color = record.color;
 	}
 	else
@@ -99,12 +86,13 @@ t_color	get_ray_color(t_parse map_info, t_ray ray)
 }
 
 /**
- * @brief point the ray in the right direction
+ * @brief point the ray in the right direction, get the color of the object 
+ * intersecting ray.dir closest to ray.origin
  * 
- * @param map_info 
- * @param i 
- * @param j 
- * @return t_color 
+ * @param map_info	parsed info from .rt map
+ * @param i 		x location of pixel to calculate color of
+ * @param j 		y location of pixel to calculate color of
+ * @return t_color 	the found color  
  */
 t_color	point_ray_get_color(t_parse map_info, float i, float j)
 {
