da1c8031f0cec4eb12a34f2914e395401e0fcaf1
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-11-11 15:35:45 +0100

    added normalize_color prototype

--- include/color.h
+++ include/color.h
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/26 14:47:08 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/11/03 22:00:46 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/11/11 15:34:24 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,15 @@ typedef struct s_color
 	unsigned int	b;
 }	t_color;
 
-typedef struct s_unit_color
-{
-	double	r;
-	double	g;
-	double	b;
-}	t_unit_color;
-
 t_color	add_colors(t_color first, t_color second);
 t_color	substract_colors(t_color first, t_color second);
 t_color	multiply_colors(t_color first, t_color second);
 t_color	multiply_color_float(t_color color, float t);
 t_color	divide_color_with_float(t_color color, float t);
-t_color	unit_vector_color(t_color dir);
 float	dot_colors(t_color first, t_color second);
+t_color	clamp_color(t_color color);
+t_color	normalize_color(t_color dir);
+
 t_color	new_color(unsigned int r, unsigned int g, unsigned int b);
 
 #endif
