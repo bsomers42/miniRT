6a28a5621172f2c81d77272427a9dc2a971fbcf0
Jaberkro <jaberkro@student.codam.nl>
2022-12-14 17:21:54 +0100

    removed 3 unused functions

--- include/color.h
+++ include/color.h
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/26 14:47:08 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/12/14 17:12:58 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/12/14 17:20:59 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,7 @@ typedef struct s_color
 }	t_color;
 
 t_color	add_colors(t_color first, t_color second);
-t_color	substract_colors(t_color first, t_color second);
-t_color	multiply_colors(t_color first, t_color second);
-t_color	multiply_color_double(t_color color, double t);
 t_color	divide_color_with_double(t_color color, double t);
-
 t_color	new_color(unsigned int r, unsigned int g, unsigned int b);
 
 #endif
--- src/color/color_math_basics.c
+++ src/color/color_math_basics.c
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/26 14:49:47 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/12/08 14:58:20 by bsomers       ########   odam.nl         */
+/*   Updated: 2022/12/14 17:21:27 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,36 +19,6 @@ t_color	add_colors(t_color first, t_color second)
 	result.r = first.r + second.r;
 	result.g = first.g + second.g;
 	result.b = first.b + second.b;
-	return (result);
-}
-
-t_color	substract_colors(t_color first, t_color second)
-{
-	t_color	result;
-
-	result.r = first.r - second.r;
-	result.g = first.g - second.g;
-	result.b = first.b - second.b;
-	return (result);
-}
-
-t_color	multiply_colors(t_color first, t_color second)
-{
-	t_color	result;
-
-	result.r = first.r * second.r;
-	result.g = first.g * second.g;
-	result.b = first.b * second.b;
-	return (result);
-}
-
-t_color	multiply_color_double(t_color color, double t)
-{
-	t_color	result;
-
-	result.r = (double)color.r * t;
-	result.g = (double)color.g * t;
-	result.b = (double)color.b * t;
 	return (result);
 }
 
